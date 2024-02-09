76fd5e5a68fc6ee7606b0c6756d69ae339652784
chonyy <mlchony7@gmail.com>
2024-02-08 20:59:14 -0500

    visual

--- src/codee.py
+++ src/codee.py
@@ -3,20 +3,9 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-
-input_text=" neck cca jdark messy hotel "
-
-with open('data/songs.json', 'r') as f:
-    try:
-        songs = json.load(f)
-    except:
-        print("Empty")
-
-# convertir todo a minúsculas y eliminar las palabras irrelevantes
-stop_words = set(stopwords.words('english'))#Las stopwords son palabras que se omiten durante el procesamiento de texto en tareas de Procesamiento del Lenguaje Natural (NLP) porque suelen no aportar mucho significado en el contexto de un texto.
 
 def preprocess_song(song):
+    stop_words = set(stopwords.words('english'))
     lyrics = song['lyrics'].lower().split()
     tokens = word_tokenize(' '.join(lyrics))
     tokens = [word for word in tokens if word not in stop_words]
@@ -24,24 +13,19 @@ def preprocess_song(song):
 
 
 def preprocess_input(input_text):
+    stop_words = set(stopwords.words('english'))
     tokens = word_tokenize(input_text.lower())
     tokens = [word for word in tokens if word not in stop_words]
     return ' '.join(tokens)
 
-preprocessed_songs = {title: preprocess_song(song) for title, song in songs.items()}
-preprocessed_text = preprocess_input(input_text)
 
-#crear los embeddings de las letras usando Word2Vec:
-sentences = [list(map(str, lyric.split())) for lyric in preprocessed_songs.values()]
-
-model = Word2Vec(sentences, window=5, min_count=1, workers=4)
-model.save("word2vec.model")
 
 #calcular la similitud entre las canciones:
-def get_song_similarities(input_text):
+def get_song_similarities(input_text,model,preprocessed_songs):
     input_tokens = preprocess_input(input_text).split()
     input_vectors = [model.wv[token] for token in input_tokens if token in model.wv]
     similarities = []
+    
     try:
         for song_title, song_lyrics in preprocessed_songs.items():
             song_tokens = preprocess_input(song_lyrics).split()
@@ -49,16 +33,33 @@ def get_song_similarities(input_text):
             similarity = cosine_similarity(input_vectors, song_vectors)[0, 0] # Obtén el valor de similitud de la matriz
             similarities.append((song_title, similarity))
         similarities.sort(key=lambda x: x[1], reverse=True)
-        
     except:
-        print("No matches")
+        print("Empty")    
         
     return similarities
 
 
 #generar recomendaciones:
 def recommend_songs(input_text):
-    similarities = get_song_similarities(input_text)
-    return [song for song, _ in similarities[:2]]
+    
+    print(input_text)
+    with open('data/songs1.json', 'r',encoding='utf-8') as f:
+        try:
+            songs = json.load(f)
+        except:
+            print("Empty")
+            
+            
+    preprocessed_songs = {title: preprocess_song(song) for title, song in songs.items()}
+    #crear los embeddings de las letras usando Word2Vec:
+    sentences = [list(map(str, lyric.split())) for lyric in preprocessed_songs.values()] 
+    model = Word2Vec(sentences, window=5, min_count=1, workers=4)
+    model.save("word2vec.model")       
+    similarities = get_song_similarities(input_text,model,preprocessed_songs)
+    return [song for song, _ in similarities[:10]]
 
+
+#test
+#esperado:Lose Yourself
+input_text="certified quality a dat da girl dem need and dem not stop cry without apology buck dem da right waydat my policy sean paul alongsidenow hear what da man say beyonce dutty ya dutty ya dutty ya beyonce sing it now ya baby boy you stay on my mind fulfill my fantasies i think about you all the time i see you in my dreams baby boy not a day goes by without my fantasies i think about you all the time i see you in my dreams ah oh my baby's fly baby oh yes no hurt me so good baby oh i'm so wrapped up in your love let me go let me breathe stay out my fantasies ya ready gimme da ting dat ya ready get ya live and tell me all about da tings that you will fantasize i know you dig da way me step da way me make my stride follow your feelings "
 print(recommend_songs(input_text))
---
+++ src/gui.py
@@ -0,0 +1,19 @@
+
+import streamlit as st
+import pandas as pd
+import codee
+
+
+st.title("Mi aplicación de recomendación de canciones")
+st.subheader("Introduce el texto que desees:")
+
+
+#  input_text es una variable que contiene el texto introducido por el usuario
+input_text = st.text_input("Text", key="name")
+
+# Calcula las canciones recomendadas basándose en el texto de entrada
+recommended_songs = codee.recommend_songs(input_text)
+
+# Muestra las canciones recomendadas en la aplicación Streamlit
+st.markdown("<h2 style='text-align: center;'>Canciónes Recomendadas</h2>", unsafe_allow_html=True)
+st.table(pd.DataFrame(recommended_songs, columns=["Nombre de la Canción"]))
